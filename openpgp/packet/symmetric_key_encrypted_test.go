6ec3c64940b69b96729033f58c3dc6ab426ee4f0
zugzwang <fvial@pm.me>
2019-10-16 18:11:35 -0300

    symmetricKeyEncrypted: Include AEAD-OCB RFC test vectors

--- openpgp/packet/symmetric_key_encrypted_test.go
+++ openpgp/packet/symmetric_key_encrypted_test.go
@@ -60,48 +60,50 @@ func TestSymmetricKeyEncrypted(t *testing.T) {
 }
 
 func TestSymmetricKeyEncryptedV5(t *testing.T) {
-	testCase := aeadEaxRFC
-	// Key
-	buf := readerFromHex(testCase.packets)
-	packet, err := Read(buf)
-	if err != nil {
-		t.Errorf("failed to read SymmetricKeyEncrypted: %s", err)
-		return
-	}
-	ske, ok := packet.(*SymmetricKeyEncrypted)
-	if !ok {
-		t.Error("didn't find SymmetricKeyEncrypted packet")
-		return
-	}
-	// Decrypt key
-	key, cipherFunc, err := ske.Decrypt([]byte("password"))
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	packet, err = Read(buf)
-	if err != nil {
-		t.Errorf("failed to read SymmetricallyEncrypted: %s", err)
-		return
-	}
-	aeadE, ok := packet.(*AEADEncrypted)
-	if !ok {
-		t.Error("didn't find SymmetricallyEncrypted packet")
-		return
-	}
-	r, err := aeadE.Decrypt(cipherFunc, key)
-	if err != nil {
-		t.Error(err)
-		return
-	}
+	testCases := []*packetSequence{aeadEaxRFC, aeadOcbRFC}
+	for _, testCase := range testCases {
+		// Key
+		buf := readerFromHex(testCase.packets)
+		packet, err := Read(buf)
+		if err != nil {
+			t.Errorf("failed to read SymmetricKeyEncrypted: %s", err)
+			return
+		}
+		ske, ok := packet.(*SymmetricKeyEncrypted)
+		if !ok {
+			t.Error("didn't find SymmetricKeyEncrypted packet")
+			return
+		}
+		// Decrypt key
+		key, cipherFunc, err := ske.Decrypt([]byte(testCase.password))
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		packet, err = Read(buf)
+		if err != nil {
+			t.Errorf("failed to read SymmetricallyEncrypted: %s", err)
+			return
+		}
+		aeadE, ok := packet.(*AEADEncrypted)
+		if !ok {
+			t.Error("didn't find SymmetricallyEncrypted packet")
+			return
+		}
+		r, err := aeadE.Decrypt(cipherFunc, key)
+		if err != nil {
+			t.Error(err)
+			return
+		}
 
-	contents, err := ioutil.ReadAll(r)
-	if err != nil && err != io.EOF && err != io.ErrUnexpectedEOF {
-		fmt.Println(err)
-		t.Error(err)
-		return
+		contents, err := ioutil.ReadAll(r)
+		if err != nil && err != io.EOF && err != io.ErrUnexpectedEOF {
+			fmt.Println(err)
+			t.Error(err)
+			return
+		}
+		fmt.Println(string(contents))
 	}
-	fmt.Println(string(contents))
 
 	// expectedContents, _ := hex.DecodeString(symmetricallyEncryptedContentsHex)
 	// if !bytes.Equal(expectedContents, contents) {
@@ -120,6 +122,11 @@ type packetSequence struct {
 var aeadEaxRFC = &packetSequence{
 	password: "password",
 	packets: "c33e0507010308cd5a9f70fbe0bc6590bc669e34e500dcaedc5b32aa2dab02359dee19d07c3446c4312a34ae1967a2fb7e928ea5b4fa8012bd456d1738c63c36d44a0107010eb732379f73c4928de25facfe6517ec105dc11a81dc0cb8a2f6f3d90016384a56fc821ae11ae8dbcb49862655dea88d06a81486801b0ff387bd2eab013de1259586906eab2476",
+}
+
+var aeadOcbRFC = &packetSequence{
+	password: "password",
+	packets: "c33d05070203089f0b7da3e5ea64779099e326e5400a90936cefb4e8eba08c6773716d1f2714540a38fcac529949dac529d3de31e15b4aeb729e330033dbedd4490107020e5ed2bc1e470abe8f1d644c7a6c8a567b0f7701196611a154ba9c2574cd056284a8ef68035c623d93cc708a43211bb6eaf2b27f7c18d571bcd83b20add3a08b73af15b9a098",
 }
 
 const symmetricallyEncryptedContentsHex = "cb1875076d73672e7478745cafc23e636f6e74656e74732e0d0a"
