984ea9801a1eec029a6dfbd4c22c5a74a266cd79
Konstantinos Andrikopoulos <mandragore@protonmail.com>
2021-07-06 20:11:18 +0200

    break symmetric.go in two files

--- openpgp/symmetric/hmac.go
+++ openpgp/symmetric/hmac.go
@@ -5,21 +5,7 @@ import (
 	"crypto"
 	"crypto/sha256"
 	"crypto/hmac"
-
-	"github.com/ProtonMail/go-crypto/openpgp/internal/algorithm"
 )
-
-type AEADPublicKey struct {
-	Cipher algorithm.CipherFunction
-	BindingHash [32]byte
-	Key []byte
-}
-
-type AEADPrivateKey struct {
-	PublicKey AEADPublicKey
-	HashSeed [32]byte
-	Key []byte
-}
 
 type HMACPublicKey struct {
 	Hash crypto.Hash
@@ -36,46 +22,6 @@ type HMACPrivateKey struct {
 	PublicKey HMACPublicKey
 	HashSeed [32]byte
 	Key []byte
-}
-
-func AEADGenerateKey(rand io.Reader, cipher algorithm.CipherFunction) (priv *AEADPrivateKey, err error) {
-	priv, err = generatePrivatePartAEAD(rand, cipher)
-	if err != nil {
-		return
-	}
-
-	priv.generatePublicPartAEAD(cipher)
-	return
-}
-
-func generatePrivatePartAEAD(rand io.Reader, cipher algorithm.CipherFunction) (priv *AEADPrivateKey, err error) {
-	priv = new(AEADPrivateKey)
-	var seed [32] byte
-	_, err = rand.Read(seed[:])
-	if err != nil {
-		return
-	}
-
-	key := make([]byte, cipher.KeySize())
-	_, err = rand.Read(key)
-	if err != nil {
-		return
-	}
-
-	priv.HashSeed = seed
-	priv.Key = key
-	return
-}
-
-func (priv *AEADPrivateKey) generatePublicPartAEAD(cipher algorithm.CipherFunction) (err error) {
-	priv.PublicKey.Cipher = cipher
-
-	bindingHash := ComputeBindingHash(priv.HashSeed)
-
-	priv.PublicKey.Key = make([]byte, len(priv.Key))
-	copy(priv.PublicKey.Key, priv.Key)
-	copy(priv.PublicKey.BindingHash[:], bindingHash)
-	return
 }
 
 func HMACGenerateKey(rand io.Reader, hash crypto.Hash) (priv *HMACPrivateKey, err error) {
@@ -123,23 +69,6 @@ func ComputeBindingHash(seed [32]byte) []byte {
 	bindingHash.Write(seed[:])
 
 	return bindingHash.Sum(nil)
-}
-
-func (pub *AEADPublicKey) Encrypt(rand io.Reader, data []byte, mode algorithm.AEADMode) (nonce []byte, ciphertext []byte, err error) {
-	block := pub.Cipher.New(pub.Key)
-	aead := mode.New(block)
-	nonce = make([]byte, aead.NonceSize())
-	rand.Read(nonce)
-	ciphertext = aead.Seal(nil, nonce, data, nil)
-	return
-}
-
-func (priv *AEADPrivateKey) Decrypt(nonce []byte, ciphertext []byte, mode algorithm.AEADMode) (message []byte, err error) {
-
-	block := priv.PublicKey.Cipher.New(priv.Key)
-	aead := mode.New(block)
-	message, err = aead.Open(nil, nonce, ciphertext, nil)
-	return
 }
 
 func (priv *HMACPrivateKey) Public() crypto.PublicKey {
