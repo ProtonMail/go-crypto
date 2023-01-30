943deefc6b6976dbd7b2627c64f1ed3d96a66481
marinthiercelin <marin.thiercelin@proton.ch>
2023-01-30 18:42:52 +0100

    Fix serialization of signature notation data (#144)

--- openpgp/packet/notation.go
+++ openpgp/packet/notation.go
@@ -3,9 +3,9 @@ package packet
 // Notation type represents a Notation Data subpacket
 // see https://tools.ietf.org/html/rfc4880#section-5.2.3.16
 type Notation struct {
-	Name string
-	Value []byte
-	IsCritical bool
+	Name            string
+	Value           []byte
+	IsCritical      bool
 	IsHumanReadable bool
 }
 
@@ -14,7 +14,7 @@ func (not *Notation) getData() []byte {
 	nameLen := len(nameData)
 	valueLen := len(not.Value)
 
-	data := make([]byte, 8 + nameLen + valueLen)
+	data := make([]byte, 8+nameLen+valueLen)
 	if not.IsHumanReadable {
 		data[0] = 0x80
 	}
@@ -23,7 +23,7 @@ func (not *Notation) getData() []byte {
 	data[5] = byte(nameLen)
 	data[6] = byte(valueLen >> 8)
 	data[7] = byte(valueLen)
-
-	data = append(data, nameData...)
-	return append(data, not.Value...)
+	copy(data[8:8+nameLen], nameData)
+	copy(data[8+nameLen:], not.Value)
+	return data
 }
