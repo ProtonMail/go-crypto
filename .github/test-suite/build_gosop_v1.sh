f598b26f1b69b83869ba5acf71fcf2fa0c8fb01e
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2023-08-16 11:29:27 +0200

    fix(CI): Update github actions with gopenpgp v3 and gosop v2

--- .github/test-suite/build_gosop_v1.sh
+++ .github/test-suite/build_gosop_v1.sh
@@ -1,5 +1,5 @@
 cd gosop
 echo "replace github.com/ProtonMail/go-crypto => ../go-crypto" >> go.mod
 go get github.com/ProtonMail/go-crypto
-go get github.com/ProtonMail/gopenpgp/v3/crypto@bebe9d408c2a75a4053de49b25638357bf92e692 
+go get github.com/ProtonMail/gopenpgp/v2/crypto@latest
 go build .
