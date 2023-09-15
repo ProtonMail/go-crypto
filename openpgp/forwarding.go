6c1edd7a1b34b3813a1b2bfaae06c2b4216d06e6
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2023-09-15 16:07:30 +0200

    feat: Add symmetric keys to v2

--- openpgp/forwarding.go
+++ openpgp/forwarding.go
@@ -134,10 +134,10 @@ func (e *Entity) NewForwardingEntity(
 		instance.ForwardeeFingerprint = forwardeeSubKey.PublicKey.Fingerprint
 
 		// 0x04 - This key may be used to encrypt communications.
-		forwardeeSubKey.Sig.FlagEncryptCommunications = true
+		forwardeeSubKey.Sig.FlagEncryptCommunications = false
 
 		// 0x08 - This key may be used to encrypt storage.
-		forwardeeSubKey.Sig.FlagEncryptStorage = true
+		forwardeeSubKey.Sig.FlagEncryptStorage = false
 
 		// 0x10 - The private component of this key may have been split by a secret-sharing mechanism.
 		forwardeeSubKey.Sig.FlagSplitKey = true
