344351235975ba58deb26853906a9e9ef2d76a2f
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2023-09-27 08:13:26 +0200

    ci: Update to latest gopenpgp v3 fot ci interop tests

--- .github/test-suite/build_gosop.sh
+++ .github/test-suite/build_gosop.sh
@@ -1,5 +1,5 @@
 cd gosop
 echo "replace github.com/ProtonMail/go-crypto => ../go-crypto" >> go.mod
 go get github.com/ProtonMail/go-crypto
-go get github.com/ProtonMail/gopenpgp/v3/crypto@aebe43356421c682a82b22760174db4bd60b2fa5 
+go get github.com/ProtonMail/gopenpgp/v3/crypto@4ec274320a814a100c71e088f66db1094497d7bb 
 go build .
