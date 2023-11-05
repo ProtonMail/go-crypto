edee42280831e90d0d2f5314bf66ba6246190bd9
Aron Wussler <aron.wussler@proton.ch>
2023-11-05 13:45:49 +0100

    Update implementation from draft v1 to v3 - Remove v6 binding for PQC KEMs - Update KDF - Update reference comments - Rename SPHINCS+ to SLH-DSA - Rename Dilithium to ML-DSA - Rename Kyber to ML-KEM - Add vectors generated with RNP - Fix misc bugs and improve tests

--- openpgp/mlkem_ecdh/mlkem_ecdh_test.go
+++ openpgp/mlkem_ecdh/mlkem_ecdh_test.go
@@ -1,26 +1,23 @@
-// Package kyber_ecdh_test tests the implementation of hybrid Kyber + ECDH encryption, suitable for OpenPGP, experimental.
-package kyber_ecdh_test
+// Package mlkem_ecdh_test tests the implementation of hybrid ML-KEM + ECDH encryption, suitable for OpenPGP, experimental.
+package mlkem_ecdh_test
 
 import (
 	"bytes"
 	"crypto/rand"
 	"github.com/ProtonMail/go-crypto/openpgp/internal/algorithm"
-	"github.com/ProtonMail/go-crypto/openpgp/kyber_ecdh"
+	"github.com/ProtonMail/go-crypto/openpgp/mlkem_ecdh"
 	"github.com/ProtonMail/go-crypto/openpgp/packet"
 	"testing"
 )
 
 func TestEncryptDecrypt(t *testing.T) {
-	randomData := make([]byte, 32)
-	rand.Read(randomData)
-
 	asymmAlgos := map[string] packet.PublicKeyAlgorithm {
-		"Kyber768_X25519": packet.PubKeyAlgoKyber768X25519,
-		"Kyber1024_X448": packet.PubKeyAlgoKyber1024X448,
-		"Kyber768_P256": packet.PubKeyAlgoKyber768P256,
-		"Kyber1024_P384":packet.PubKeyAlgoKyber1024P384,
-		"Kyber768_Brainpool256": packet.PubKeyAlgoKyber768Brainpool256,
-		"Kyber1024_Brainpool384":packet.PubKeyAlgoKyber1024Brainpool384,
+		"Mlkem768_X25519": packet.PubKeyAlgoMlkem768X25519,
+		"Mlkem1024_X448": packet.PubKeyAlgoMlkem1024X448,
+		"Mlkem768_P256": packet.PubKeyAlgoMlkem768P256,
+		"Mlkem1024_P384":packet.PubKeyAlgoMlkem1024P384,
+		"Mlkem768_Brainpool256": packet.PubKeyAlgoMlkem768Brainpool256,
+		"Mlkem1024_Brainpool384":packet.PubKeyAlgoMlkem1024Brainpool384,
 	}
 
 	symmAlgos := map[string] algorithm.Cipher {
@@ -34,7 +31,7 @@ func TestEncryptDecrypt(t *testing.T) {
 			key := testGenerateKeyAlgo(t, asymmAlgo)
 			for symmName, symmAlgo := range symmAlgos {
 				t.Run(symmName, func(t *testing.T) {
-					testEncryptDecryptAlgo(t, key, randomData, symmAlgo)
+					testEncryptDecryptAlgo(t, key, symmAlgo)
 				})
 			}
 			testvalidateAlgo(t, asymmAlgo)
@@ -45,45 +42,45 @@ func TestEncryptDecrypt(t *testing.T) {
 func testvalidateAlgo(t *testing.T, algId packet.PublicKeyAlgorithm) {
 	var err error
 	key := testGenerateKeyAlgo(t, algId)
-	if err := kyber_ecdh.Validate(key); err != nil {
+	if err := mlkem_ecdh.Validate(key); err != nil {
 		t.Fatalf("valid key marked as invalid: %s", err)
 	}
 
-	bin, _ := key.PublicKyber.MarshalBinary()
+	bin, _ := key.PublicMlkem.MarshalBinary()
 	bin[5] ^= 1
-	key.PublicKyber, err = key.Kyber.UnmarshalBinaryPublicKey(bin)
+	key.PublicMlkem, err = key.Mlkem.UnmarshalBinaryPublicKey(bin)
 	if err != nil {
 		t.Fatal("unable to corrupt key")
 	}
 
-	if err := kyber_ecdh.Validate(key); err == nil {
+	if err := mlkem_ecdh.Validate(key); err == nil {
 		t.Fatalf("failed to detect invalid key")
 	}
 
 	// Generate fresh key
 	key = testGenerateKeyAlgo(t, algId)
-	if err := kyber_ecdh.Validate(key); err != nil {
+	if err := mlkem_ecdh.Validate(key); err != nil {
 		t.Fatalf("valid key marked as invalid: %s", err)
 	}
 	
 	key.PublicPoint[5] ^= 1
-	if err := kyber_ecdh.Validate(key); err == nil {
+	if err := mlkem_ecdh.Validate(key); err == nil {
 		t.Fatalf("failed to detect invalid key")
 	}
 }
 
-func testGenerateKeyAlgo(t *testing.T, algId packet.PublicKeyAlgorithm) *kyber_ecdh.PrivateKey {
+func testGenerateKeyAlgo(t *testing.T, algId packet.PublicKeyAlgorithm) *mlkem_ecdh.PrivateKey {
 	curveObj, err := packet.GetECDHCurveFromAlgID(algId)
 	if err != nil {
 		t.Errorf("error getting curve: %s", err)
 	}
 
-	kyberObj, err := packet.GetKyberFromAlgID(algId)
+	kyberObj, err := packet.GetMlkemFromAlgID(algId)
 	if err != nil {
 		t.Errorf("error getting kyber: %s", err)
 	}
 
-	priv, err := kyber_ecdh.GenerateKey(rand.Reader, uint8(algId), curveObj, kyberObj)
+	priv, err := mlkem_ecdh.GenerateKey(rand.Reader, uint8(algId), curveObj, kyberObj)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -91,16 +88,16 @@ func testGenerateKeyAlgo(t *testing.T, algId packet.PublicKeyAlgorithm) *kyber_e
 	return priv
 }
 
-func testEncryptDecryptAlgo(t *testing.T, priv *kyber_ecdh.PrivateKey, publicKeyHash []byte, kdfCipher algorithm.Cipher) {
+func testEncryptDecryptAlgo(t *testing.T, priv *mlkem_ecdh.PrivateKey, kdfCipher algorithm.Cipher) {
 	expectedMessage := make([]byte, kdfCipher.KeySize()) // encryption algo + checksum
 	rand.Read(expectedMessage)
 
-	kE, ecE, c, err := kyber_ecdh.Encrypt(rand.Reader, &priv.PublicKey, expectedMessage, publicKeyHash)
+	kE, ecE, c, err := mlkem_ecdh.Encrypt(rand.Reader, &priv.PublicKey, expectedMessage)
 	if err != nil {
 		t.Errorf("error encrypting: %s", err)
 	}
 
-	decryptedMessage, err := kyber_ecdh.Decrypt(priv, kE, ecE, c, publicKeyHash)
+	decryptedMessage, err := mlkem_ecdh.Decrypt(priv, kE, ecE, c)
 	if err != nil {
 		t.Errorf("error decrypting: %s", err)
 	}
