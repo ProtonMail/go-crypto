6a8c5cde2538503a81e0a4ae56c8e66007b272e2
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2024-01-18 10:14:55 +0100

    ci: Update CI to compare v1 and v2 in the interoperability test

--- .github/test-suite/build_gosop.sh
+++ .github/test-suite/build_gosop.sh
@@ -1,5 +1,5 @@
 cd gosop
 echo "replace github.com/ProtonMail/go-crypto => ../go-crypto" >> go.mod
 go get github.com/ProtonMail/go-crypto
-go get github.com/ProtonMail/gopenpgp/v3/crypto@8acccb3915b46d8765d536ff9669bb61ec567f77 
+go get github.com/ProtonMail/gopenpgp/v3/crypto@80762a9ce60ba09d8a0d4f7b2a9a9665e7716351 
 go build .
--- .github/test-suite/prepare_config.sh
+++ .github/test-suite/prepare_config.sh
@@ -2,11 +2,13 @@ CONFIG_TEMPLATE=$1
 CONFIG_OUTPUT=$2
 GOSOP_BRANCH_V1=$3
 GOSOP_BRANCH_V2=$4
-GOSOP_MAIN=$5
+GOSOP_MAIN_V1=$5
+GOSOP_MAIN_V2=$6
 cat $CONFIG_TEMPLATE \
     | sed "s@__GOSOP_BRANCH_V1__@${GOSOP_BRANCH_V1}@g" \
     | sed "s@__GOSOP_BRANCH_V2__@${GOSOP_BRANCH_V2}@g" \
-    | sed "s@__GOSOP_MAIN__@${GOSOP_MAIN}@g" \
+    | sed "s@__GOSOP_MAIN_V1__@${GOSOP_MAIN_V1}@g" \
+    | sed "s@__GOSOP_MAIN_V2__@${GOSOP_MAIN_V2}@g" \
     | sed "s@__SQOP__@${SQOP}@g" \
     | sed "s@__GPGME_SOP__@${GPGME_SOP}@g" \
     | sed "s@__SOP_OPENPGPJS__@${SOP_OPENPGPJS}@g" \
