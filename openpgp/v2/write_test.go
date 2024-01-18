65ad1e4b3d23861cb105f32af236e05dc35beeda
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2024-01-18 15:33:40 +0100

    Replace ioutil.ReadAll with io.ReadAll

--- openpgp/v2/write_test.go
+++ openpgp/v2/write_test.go
@@ -1036,7 +1036,10 @@ func TestEncryptWithAEAD(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	dec, err := ioutil.ReadAll(m.decrypted)
+	dec, err := io.ReadAll(m.decrypted)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if !bytes.Equal(dec, []byte(message)) {
 		t.Error("decrypted does not match original")
