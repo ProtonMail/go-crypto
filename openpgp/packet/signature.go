728b0bb77c4e5b516cd638aa528fc0cc51ab436a
Thanasis <thanasis@silverflow.com>
2024-03-07 16:54:37 +0100

    add support for signature subpacket type 4 (recoverable flag)

--- openpgp/packet/signature.go
+++ openpgp/packet/signature.go
@@ -80,6 +80,10 @@ type Signature struct {
 	Notations                                               []*Notation
 	IntendedRecipients                                      []*Recipient
 
+	// Recoverable is a flag to indicate whenether the signature is revocable or not.
+	// See RFC 4880, section 5.2.3.12 for details.
+	Recoverable *bool
+
 	// TrustLevel and TrustAmount can be set by the signer to assert that
 	// the key is not only valid but also trustworthy at the specified
 	// level.
@@ -331,6 +335,7 @@ type signatureSubpacketType uint8
 const (
 	creationTimeSubpacket        signatureSubpacketType = 2
 	signatureExpirationSubpacket signatureSubpacketType = 3
+	recoverable                  signatureSubpacketType = 4
 	trustSubpacket               signatureSubpacketType = 5
 	regularExpressionSubpacket   signatureSubpacketType = 6
 	keyExpirationSubpacket       signatureSubpacketType = 9
@@ -418,6 +423,11 @@ func parseSignatureSubpacket(sig *Signature, subpacket []byte, isHashed bool) (r
 		}
 		sig.SigLifetimeSecs = new(uint32)
 		*sig.SigLifetimeSecs = binary.BigEndian.Uint32(subpacket)
+	case recoverable:
+		sig.Recoverable = new(bool)
+		if subpacket[0] > 0 {
+			*sig.Recoverable = true
+		}
 	case trustSubpacket:
 		if len(subpacket) != 2 {
 			err = errors.StructuralError("trust subpacket with bad length")
