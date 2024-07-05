3df78a3ea124d4f5ed9ec02d14e78731282dc15a
Daniel Huigens <d.huigens@protonmail.com>
2024-07-05 10:36:53 +0200

    Restrict S2K types (#213)
    RFC9580 says that:
    
        Argon2 is only used with AEAD (S2K usage octet 253).  An
        implementation MUST NOT create and MUST reject as malformed any
        secret key packet where the S2K usage octet is not AEAD (253) and
        the S2K specifier type is Argon2.
    
    Therefore, we disallow reading and writing Argon2 keys without AEAD.
    
    And:
    
        [The Simple and Salted S2K methods] are used only for reading in
        backwards compatibility mode.
        
    So, we disallow encrypting keys using those methods.
    
    Since v6 keys don't need backwards compatibility, we also disallow
    reading Simple S2K there. We still allow reading Salted S2K since the
    spec says it may be used "when [the password] is high entropy".

--- openpgp/packet/private_key.go
+++ openpgp/packet/private_key.go
@@ -261,6 +261,12 @@ func (pk *PrivateKey) parse(r io.Reader) (err error) {
 		if pk.s2kParams.Dummy() {
 			return
 		}
+		if pk.s2kParams.Mode() == s2k.Argon2S2K && pk.s2kType != S2KAEAD {
+			return errors.StructuralError("using Argon2 S2K without AEAD is not allowed")
+		}
+		if pk.s2kParams.Mode() == s2k.SimpleS2K && pk.Version == 6 {
+			return errors.StructuralError("using Simple S2K with version 6 keys is not allowed")
+		}
 		pk.s2k, err = pk.s2kParams.Function()
 		if err != nil {
 			return
@@ -653,6 +659,13 @@ func (pk *PrivateKey) encrypt(key []byte, params *s2k.Params, s2kType S2KType, c
 	// check if encryptionKey has the correct size
 	if len(key) != cipherFunction.KeySize() {
 		return errors.InvalidArgumentError("supplied encryption key has the wrong size")
+	}
+
+	if params.Mode() == s2k.Argon2S2K && s2kType != S2KAEAD {
+		return errors.InvalidArgumentError("using Argon2 S2K without AEAD is not allowed")
+	}
+	if params.Mode() != s2k.Argon2S2K && params.Mode() != s2k.IteratedSaltedS2K {
+		return errors.InvalidArgumentError("insecure S2K mode")
 	}
 
 	priv := bytes.NewBuffer(nil)
