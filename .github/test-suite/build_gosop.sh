b5837fa4476e00841adaacdb8253b7ee59fa968f
Lukas Burkhalter <10532077+lubux@users.noreply.github.com>
2024-08-27 13:14:22 +0200

    ci: Change gosop branch for gopenpgp-v2 to gosop-gopenpgp-v2 (#224)

--- .github/test-suite/build_gosop.sh
+++ .github/test-suite/build_gosop.sh
@@ -1,5 +1,5 @@
 cd gosop
 echo "replace github.com/ProtonMail/go-crypto => ../go-crypto" >> go.mod
 go get github.com/ProtonMail/go-crypto
-go get github.com/ProtonMail/gopenpgp/v3/crypto@db2db2c2cd366696183a2d3cf6fea63eb679e54c 
+go get github.com/ProtonMail/gopenpgp/v3/crypto@c09849a05e9a6221a99bc1dd8731f36d16ea1757 
 go build .
