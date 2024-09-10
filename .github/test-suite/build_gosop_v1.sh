ccdb9fb6d1cfe23d670a0dbcc69ffd0337cc94c8
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2024-09-10 13:05:36 +0200

    fix: Fix CI for v1 interoptest

--- .github/test-suite/build_gosop_v1.sh
+++ .github/test-suite/build_gosop_v1.sh
@@ -1,5 +1,5 @@
 cd gosop
 echo "replace github.com/ProtonMail/go-crypto => ../go-crypto" >> go.mod
 go get github.com/ProtonMail/go-crypto
-go get github.com/ProtonMail/gopenpgp/v2/crypto@latest
+go get github.com/ProtonMail/gopenpgp/v2/crypto@v2.8.0-alpha.1
 go build .
