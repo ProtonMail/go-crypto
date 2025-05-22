bef6029cf6f1556d4e31b8ae0fc370707c0f375e
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2025-05-22 11:59:34 +0200

    Update default config

--- openpgp/packet/config.go
+++ openpgp/packet/config.go
@@ -449,8 +449,10 @@ func (c *Config) GenerateNonCriticalSignatureCreationTime() bool {
 }
 
 func (c *Config) DecompressedMessageSizeLimit() *int64 {
-	if c == nil {
-		return nil
+	if c == nil || c.MaxDecompressedMessageSize == nil {
+		// 50 MiB
+		max := 50 * (int64(1) << 20)
+		return &max
 	}
 	return c.MaxDecompressedMessageSize
 }
