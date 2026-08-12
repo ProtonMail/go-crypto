3b0df7a9fd2869239887a44826f1f1e95e3b2291
knQzx <75641500+knQzx@users.noreply.github.com>
2026-08-12 10:02:09 +0200

    guard nil issuer key id on v2 message read path

--- openpgp/v2/read.go
+++ openpgp/v2/read.go
@@ -347,6 +347,7 @@ func (sc *SignatureCandidate) validate() bool {
 		sc.SigType == correspondingSig.SigType &&
 		sc.HashAlgorithm == correspondingSig.Hash &&
 		sc.PubKeyAlgo == correspondingSig.PubKeyAlgo &&
+		correspondingSig.IssuerKeyId != nil &&
 		sc.IssuerKeyId == *correspondingSig.IssuerKeyId
 }
 
@@ -393,6 +394,9 @@ FindLiteralData:
 			md.SignatureCandidates = append([]*SignatureCandidate{sigCandidate}, md.SignatureCandidates...)
 		case *packet.Signature:
 			// Old style signature i.e., sig | literal
+			if p.IssuerKeyId == nil {
+				return nil, errors.StructuralError("signature doesn't have an issuer")
+			}
 			sigCandidate := newSignatureCandidateFromSignature(p)
 			md.IsSigned = true
 			if keyring != nil {
